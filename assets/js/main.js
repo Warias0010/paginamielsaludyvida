6fbaa264d8ad876f5b0b79073573a1ec291030db
Warias0010 <antonwalter08@gmail.com>
2021-03-18 20:55:49 -0600

    updatepage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,26 @@
-let click = document.getElementById('menu-click');
-let enlaces = document.querySelector('.enlaces-header');
+let slider = document.querySelector('.slider-contenedor');
+let sliderInd = document.querySelectorAll('.slider-test');
+let contador = 1;
 
-click.addEventListener('click', () => {
-    enlaces.classList.toggle('click');
-    
-});
+let tamanoWidth = sliderInd[0].clientWidth;
+
+setInterval(function tiempo() {
+    slide();
+}, 3000);
+
+function slide() {
+    slider.style.transform = `translate(${-tamanoWidth * contador}px)`
+    slider.style.transition = 'transform .1s';
+    contador++;
+    if (contador === sliderInd.length) {
+        
+        setTimeout(() => {
+            slider.style.transform = `translate(0px)`;
+            slider.style.transition = ' all transform 0s';
+            contador = 0;
+        }, 3600)
+        
+    }
+
+    console.log(slider.style.transform);
+}
